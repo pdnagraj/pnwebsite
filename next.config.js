cbe3d54c514ca4f64b42722b217b5f43dfebe9fd
PN <parth.d.nagraj@gmail.com>
2025-12-15 13:14:16 -0500

    test2

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
     output: "export",
     //basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-    // images: {
-    //     unoptimized: true,
-    // },
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
